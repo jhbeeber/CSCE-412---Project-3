cc499c787a9e12ea07b987f89dbaefb034ab8d69
James Beeber <jhbeeber@gmail.com>
2024-06-15 22:52:43 -0500

    Created Webserver header file for processing and queuing requests

---
+++ Webserver.h
@@ -0,0 +1,24 @@
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+#include "Request.h"
+#include <queue>
+#include <mutex>
+using namespace std;
+
+extern mutex mutexServerOutput;
+
+class Webserver {
+    public:
+        Webserver(int serverID);
+        void processRequest(const Request& request);
+        void processQueue();
+        bool isFree() const;
+
+    private:
+        int serverID;
+        bool free;
+        queue<Request> queueRequest;
+};
+
+#endif
